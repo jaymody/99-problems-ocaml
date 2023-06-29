48aa81956778f4c9eaad2edbeb49e8ceec1042b9
jaymody <jaykmody@gmail.com>
2023-06-29 19:13:59 -0400

    Solution to problem 9.

---
+++ p09.ml
@@ -0,0 +1,29 @@
+let pack list =
+  let rec inner l acc chr =
+    match (l, acc) with
+    | h :: t, _ -> if h = chr then inner t (h :: acc) chr else (l, acc)
+    | _ -> (l, acc)
+  in
+  let rec outer l acc =
+    match (l, acc) with
+    | h :: t, _ ->
+        let a, b = inner t [ h ] h in
+        outer a (b :: acc)
+    | [], _ -> acc
+  in
+  List.rev (outer list [])
+;;
+
+assert (
+  pack
+    [
+      "a"; "a"; "a"; "a"; "b"; "c"; "c"; "a"; "a"; "d"; "d"; "e"; "e"; "e"; "e";
+    ]
+  = [
+      [ "a"; "a"; "a"; "a" ];
+      [ "b" ];
+      [ "c"; "c" ];
+      [ "a"; "a" ];
+      [ "d"; "d" ];
+      [ "e"; "e"; "e"; "e" ];
+    ])
