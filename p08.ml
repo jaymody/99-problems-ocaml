c494f246aec15c44364a1523f26436c2013dad47
jaymody <jaykmody@gmail.com>
2023-06-29 15:42:16 -0400

    Solution to problem 8.

---
+++ p08.ml
@@ -0,0 +1,26 @@
+let head = function x :: _ -> Some x | [] -> None
+
+let compress list =
+  let rec fn old_list new_list =
+    match (old_list, new_list) with
+    | h :: t, _ ->
+        fn t (if Some h = head new_list then new_list else h :: new_list)
+    | _ -> new_list
+  in
+  List.rev (fn list [])
+;;
+
+assert (compress [] = []);;
+assert (compress [ 0 ] = [ 0 ]);;
+assert (compress [ 0; 0 ] = [ 0 ]);;
+assert (compress [ 0; 0; 1; 0; 0 ] = [ 0; 1; 0 ]);;
+assert (compress [ 0; 1; 0; 1; 0 ] = [ 0; 1; 0; 1; 0 ]);;
+
+assert (
+  compress
+    [ "a"; "a"; "a"; "a"; "b"; "c"; "c"; "a"; "a"; "d"; "e"; "e"; "e"; "e" ]
+  = [ "a"; "b"; "c"; "a"; "d"; "e" ])
+;;
+
+(* makes sure our implementation is efficient enough to run on a really long list *)
+assert (compress (List.init 10000000 (fun x -> 0)) = [ 0 ])
