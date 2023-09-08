31dfe8b106cc84285db962207581551b6bac350e
jaymody <jaykmody@gmail.com>
2023-09-08 19:27:36 -0400

    Remove unnecessary nesting.

--- src/p55.ml
+++ src/p55.ml
@@ -4,29 +4,28 @@ type 'a tree =
 
 let cartesian_product a b = List.concat_map (fun x -> List.map (fun y -> x, y) b) a
 
-let cbal_tree =
+let rec cbal_tree =
   let make l r = Node ('x', l, r) in
-  let rec aux acc = function
-    | 0 -> [ Empty ]
-    | 1 -> [ make Empty Empty ]
-    | n ->
-      (match n - 1 with
-       | n when n mod 2 = 0 ->
-         let subtrees = aux acc (n / 2) in
-         List.fold_left
-           (fun acc (a, b) -> make a b :: acc)
-           acc
-           (cartesian_product subtrees subtrees)
-       | n when n mod 2 = 1 ->
-         let subtrees1 = aux acc (n / 2) in
-         let subtrees2 = aux acc (n - (n / 2)) in
-         List.fold_left
-           (fun acc (a, b) -> make a b :: make b a :: acc)
-           acc
-           (cartesian_product subtrees1 subtrees2)
-       | _ -> invalid_arg "unreachable")
-  in
-  aux []
+  let acc = [] in
+  function
+  | 0 -> [ Empty ]
+  | 1 -> [ make Empty Empty ]
+  | n ->
+    (match n - 1 with
+     | n when n mod 2 = 0 ->
+       let subtrees = cbal_tree (n / 2) in
+       List.fold_left
+         (fun acc (a, b) -> make a b :: acc)
+         acc
+         (cartesian_product subtrees subtrees)
+     | n when n mod 2 = 1 ->
+       let subtrees1 = cbal_tree (n / 2) in
+       let subtrees2 = cbal_tree (n - (n / 2)) in
+       List.fold_left
+         (fun acc (a, b) -> make a b :: make b a :: acc)
+         acc
+         (cartesian_product subtrees1 subtrees2)
+     | _ -> invalid_arg "unreachable")
 ;;
 
 let%test _ = cbal_tree 0 = [ Empty ]
