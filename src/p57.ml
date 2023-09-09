70a7eada6546137dcf26edbb8eec839b747b320a
jaymody <jaykmody@gmail.com>
2023-09-08 20:18:30 -0400

    Solution for problem 57.

---
+++ src/p57.ml
@@ -0,0 +1,13 @@
+open P56
+
+let construct list =
+  let rec add t x' =
+    match t with
+    | Empty -> Node (x', Empty, Empty)
+    | Node (x, l, r) -> if x' < x then Node (x, add l x', r) else Node (x, l, add r x')
+  in
+  List.fold_left add Empty list
+;;
+
+let%test _ = is_symmetric (construct [ 5; 3; 18; 1; 4; 12; 21 ])
+let%test _ = not (is_symmetric (construct [ 3; 2; 5; 7; 4 ]))
